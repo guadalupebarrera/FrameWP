e628694b6eafcd7e43f051a9e9aea46d4e71edaf
nathanguadalupe <79476800+nathanguadalupe@users.noreply.github.com>
2021-02-24 14:59:22 -0600

    Update landing-page.php

--- landing-page.php
+++ landing-page.php
@@ -8,8 +8,33 @@
 	$menubar = $framewp_option['show_menubar'];
 	$scrollbars = $framewp_option['show_scrollbars'];
 	$resizable = $framewp_option['resizable_window'];
+	
+	if (isset($toolbar) && $toolbar === 'show_toolbar') {
+		$toolbar = 'yes';
+	} else {
+		$toolbar = 'no';
+	}
+
+	if (isset($menubar) && $menubar === 'show_menubar') {
+		$menubar = 'yes';
+	} else {
+		$menubar = 'no';
+	}
+
+	if (isset($scrollbars) && $scrollbars === 'show_scrollbars') {
+		$scrollbars = 'yes';
+	} else {
+		$scrollbars = 'no';
+	}
+
+	if (isset($resizable) && $resizable === 'resizable_window') {
+		$resizable = 'yes';
+	} else {
+		$resizable = 'no';
+	}
 
 	$img_url = $framewp_option['landing_page_img_url'];
+
 	list($img_width, $img_height, $img_type, $attr) = getimagesize($img_url);
 
 ?>
@@ -32,7 +57,14 @@ $(document).ready(function(){
 <body>
 
 <div class="parent">
-	<div class="child"><?php if(!isset($img_url)) { echo "<a href=\"link\" onclick=\"javascript:window.open('" . get_content_page_url() . "','Windows','width=+screen.availWidth+,height=+screen.availHeight,toolbar=" . esc_attr($toolbar) . ",menubar=" . esc_attr($menubar) . ",scrollbars=" . esc_attr($scrollbars) . ",resizable=" . esc_attr($resizable) . ",location=no,directories=no,status=no');return false\" )\"=\"\"><img src=\"" . $img_url . "\" width=\"" . $img_width . "\" height=\"" . $img_height . "\" alt=\"Enter\" title=\"Enter " . $website_title . "\" /></a>"; } else { echo "<h1>" . $website_title . "</h1>"; } ?>
+	<div class="child">
+	<?php 
+		if (!isset($img_url) || $img_url === '') { 
+			echo "<h1>" . $website_title . "</h1>";
+		} else { 
+			echo "<a href=\"link\" onclick=\"javascript:window.open('" . get_content_page_url() . "','Windows','width=+screen.availWidth+,height=+screen.availHeight,toolbar=" . esc_attr($toolbar) . ",menubar=" . esc_attr($menubar) . ",scrollbars=" . esc_attr($scrollbars) . ",resizable=" . esc_attr($resizable) . ",location=no,directories=no,status=no');return false\" )\"=\"\"><img src=\"" . $img_url . "\" width=\"" . $img_width . "\" height=\"" . $img_height . "\" alt=\"Enter\" title=\"Enter " . $website_title . "\" /></a>";
+		} 
+	?>
 	<?php echo "<a id=\"enter-button\" href=\"link\" onclick=\"javascript:window.open('" . get_content_page_url() . "','Windows','width=+screen.availWidth+,height=+screen.availHeight,toolbar=" . esc_attr($toolbar) . ",menubar=" . esc_attr($menubar) . ",scrollbars=" . esc_attr($scrollbars) . ",resizable=" . esc_attr($resizable) . ",location=no,directories=no,status=no');return false\" )\"=\"\">Enter</a>"; ?>
 	</div>
 </div>
